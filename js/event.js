519ff9d93c48c4d2941597f6d1b969852ac2669e
Norge Santana <tito.cubanito@gmail.com>
2022-02-04 15:23:33 -0300

    4ta Entrega: Pre-entrega

--- js/event.js
+++ js/event.js
@@ -1,45 +1,67 @@
-//Entrega 02
+//Entrega 04
 //E-commerce
 
-var accesorio,precio,descuento,operacion1,operacion2,operacion3,iva,compra,pagar,cantidad,porcentaje;
+const categorias = ["ropa", "accesorio", "calzado"];
+class Item {
+  constructor(id, nombre, precio, categoria) {
+    this.id = parseInt(id);
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio) * 1.21;
+    this.categoria = categoria.toUpperCase();
+  }
+}
 
-//Entrada Prompt
-accesorio = prompt ('Ingresar Accesorio');
-precio = parseFloat(prompt('Ingresar Precio'));
-cantidad = parseFloat(prompt("Ingresar Cantidad"));
+const products = [];
 
-//Descuento e Impuestos
-descuento = 0.30;
-iva = 0.21;
+products.push(new Item(1, "Jeans", 3000, categorias[0]));
+products.push(new Item(2, "Camisa", 1500, categorias[0]));
+products.push(new Item(3, "Billetera", 2000, categorias[1]));
+products.push(new Item(4, "Cinturon", 1700, categorias[1]));
+products.push(new Item(5, "Zapatillas", 5000, categorias[2]));
+products.push(new Item(6, "Lentes", 7000, categorias[1]));
+products.push(new Item(7, "Ojotas", 2700, categorias[2]));
+console.log(products);
 
-//Operaciones
-compra = precio * cantidad;
+//Filtro Categoria
+let userFilter1 = prompt("FILTRO: SELECCIONE CATEGORIA\n\nRopa\nAccesorio\nCalzado");
+let filtrados = products.filter((Item) => Item.categoria == userFilter1.toUpperCase());
+console.log(filtrados);
 
-operacion1 = compra * descuento;
-operacion2 = compra - operacion1; //Descuento
-operacion3 = operacion2 * iva; //IVA
+let mensaje1 = "Los Productos Encontrados son:\n\n";
 
-pagar = operacion2 + operacion3;
+if (filtrados.length > 0) {
+  for (const Item of filtrados) {
+    mensaje1 =
+      mensaje1 +
+      "Producto: " +
+      Item.nombre +
+      "\nPrecio: $" +
+      Item.precio +
+      "\n\n";
+  }
+  alert(mensaje1);
+} else {
+  alert("Producto no encontrado");
+}
 
-total = pagar.toFixed(2);
-descuento = operacion1.toFixed(2);
-iva = operacion3.toFixed(2);
+//Filtro Precio
+let userFilter2 = prompt("FILTRO: Precio menor o igual\n\nIngrese monto");
+const cheap = products.filter((Item) => Item.precio <= userFilter2);
+console.log(cheap);
 
-//Salida
-alert('Descuento (30%): -$'+ descuento + '\nI.V.A. (21%): $'+ iva + '\nTotal a pagar: $'+ total + '\n\n✨ Gracias por su compra! ✨');
+let mensaje2 = "Los Productos Encontrados son:\n\n";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (cheap.length > 0) {
+  for (const Item of cheap) {
+    mensaje2 =
+      mensaje2 +
+      "Producto: " +
+      Item.nombre +
+      "\nPrecio: $" +
+      Item.precio +
+      "\n\n";
+  }
+  alert(mensaje2);
+} else {
+  alert("Producto no encontrado");
+}
